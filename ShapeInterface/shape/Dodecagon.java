75e0e040107820d531202f7b1b437435d9fbbcd3
Rohan2026 <143441935+Rohan2026@users.noreply.github.com>
2024-01-03 19:18:20 -0800

    Add files via upload

---
+++ ShapeInterface/shape/Dodecagon.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class Dodecagon here.
+ * Its got 12 sides
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Dodecagon implements Shape
+{
+  private String color;
+  private int sidelength;
+  public Dodecagon(String color, int sidelength)
+  {
+      this.color=color;
+      this.sidelength=sidelength;
+  }
+     /**
+   * Computes the area of the shape
+   * 
+   * @return    The computed area
+   */
+  public double getArea()
+  {
+      return 3 * (2 + Math.sqrt(3)) * this.sidelength * this.sidelength;
+  }
+
+  /**
+   * Computes the perimeter of the shape
+   * 
+   * @return    The computed perimeter
+   */
+  public double getPerimeter()
+  {
+      return this.sidelength * 12;
+  }
+
+  /**
+   * Gets the shape's color
+   * 
+   * @return    The color
+   */
+  public String getColor()
+  {
+      return this.color;
+  }
+}
